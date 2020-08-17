05f3a367c2d0669ae731650fa5365e9810b985b8
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-17 22:09:00 +0900

    프래그먼트 xml / 코틀린파일 연결

--- app/src/main/java/com/kakao/fragment_20200817/fragments/FirstFragment.kt
+++ app/src/main/java/com/kakao/fragment_20200817/fragments/FirstFragment.kt
@@ -1,9 +1,21 @@
 package com.kakao.fragment_20200817.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kakao.fragment_20200817.R
 
 class FirstFragment : Fragment() {
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
 
 }
